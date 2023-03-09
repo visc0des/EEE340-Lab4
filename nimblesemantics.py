9ed467f966a561fd4b5c0d888900e85f2139dff9
Aaron Brown <snakeskipper@proton.me>
2023-03-09 16:04:25 -0500

    updated exitParameterDef and exitVarDec to make it more DRY

--- nimblesemantics.py
+++ nimblesemantics.py
@@ -84,9 +84,8 @@ class InferTypesAndCheckConstraints(NimbleListener):
         self.current_scope = self.current_scope.enclosing_scope
         # assign function type in the global scope
 
-    def exitParameterDef(self, ctx:NimbleParser.ParameterDefContext):
-        # Create parameter symbol in the current scope (function scope)
-        # Should be the same as the var dec
+    def subVarDec(self, ctx):
+        # Creating mini-lookup dictionary for verification
         type_dict = {'Int': PrimitiveType.Int, 'Bool': PrimitiveType.Bool, 'String': PrimitiveType.String}
 
         # Extracting variable type declared, its primitive type,
@@ -100,10 +99,21 @@ class InferTypesAndCheckConstraints(NimbleListener):
             self.current_scope.define(this_ID, PrimitiveType.ERROR, False)
             self.error_log.add(ctx, Category.DUPLICATE_NAME, f"Previously declared variable already has name"
                                                              f"[{this_ID}]. No duplicates are allowed.")
-            return
+            error = True
+        else:
+            error = False
+
+        return var_text, var_primtype, this_ID, error
+
+    def exitParameterDef(self, ctx:NimbleParser.ParameterDefContext):
+        # TODO make methods to clean up exitVarDec
+        # Create parameter symbol in the current scope (function scope)
+        # Should be the same as the var dec
+        var_text, var_primtype, this_ID, error = self.subVarDec(ctx)
 
         # create the symbol with the inuptted typeset the variable type accordingly
-        self.current_scope.define(this_ID, var_primtype, False)
+        if not error:
+            self.current_scope.define(this_ID, var_primtype, False)
 
     def exitReturn(self, ctx:NimbleParser.ReturnContext):
         # must match the function definition's type Will create an error in the error log
@@ -157,21 +167,7 @@ class InferTypesAndCheckConstraints(NimbleListener):
     # --------------------------------------------------------
 
     def exitVarDec(self, ctx: NimbleParser.VarDecContext):
-        # Creating mini-lookup dictionary for verification
-        type_dict = {'Int': PrimitiveType.Int, 'Bool': PrimitiveType.Bool, 'String': PrimitiveType.String}
-
-        # Extracting variable type declared, its primitive type,
-        # and the ID declared
-        var_text = ctx.TYPE().getText()
-        var_primtype = type_dict[var_text]
-        this_ID = ctx.ID().getText()
-
-        # First thing to check is if we're declaring a duplicated variable name. Set ERROR if so and stop function.
-        if self.current_scope.resolve(this_ID) is not None:
-            self.current_scope.define(this_ID, PrimitiveType.ERROR, False)
-            self.error_log.add(ctx, Category.DUPLICATE_NAME, f"Previously declared variable already has name"
-                                                             f"[{this_ID}]. No duplicates are allowed.")
-            return
+        var_text, var_primtype, this_ID, error = self.subVarDec(ctx)
 
         # If no duplicate name, and if there was an assignment,
         # check if does not violate type constraint
@@ -190,7 +186,8 @@ class InferTypesAndCheckConstraints(NimbleListener):
                 return
 
         # If all input conditions met, create the symbol with the inuptted typeset the variable type accordingly
-        self.current_scope.define(this_ID, var_primtype, False)
+        if not error:
+            self.current_scope.define(this_ID, var_primtype, False)
 
     # --------------------------------------------------------
     # Statements
